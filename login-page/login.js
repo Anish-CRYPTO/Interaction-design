fdf9c4e8627c05e171a7e64ce92a5421a5f3887d
Anish-CRYPTO <302887013@student.rocmondriaan.nl>
2021-06-25 10:36:04 +0200

    login form

---
+++ login-page/login.js
@@ -0,0 +1,14 @@
+let Username = document.getElementById('UserName').Value;
+let Password = document.getElementById('Wachtwoord').Value;
+let button = document.getElementById('Login');
+
+button.addEventListener("click",
+function()
+{
+    alert(document.getElementById('UserName').value);
+
+})
+
+
+
+
