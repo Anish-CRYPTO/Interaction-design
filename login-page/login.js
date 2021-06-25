a7ece5ea70adeba570eb8b80c08915037d58cd30
Anish-CRYPTO <302887013@student.rocmondriaan.nl>
2021-06-25 10:46:34 +0200

    added location.href

--- login-page/login.js
+++ login-page/login.js
@@ -1,13 +1,19 @@
 let Username = document.getElementById('UserName').Value;
 let Password = document.getElementById('Wachtwoord').Value;
 let button = document.getElementById('Login');
+let adminGebruikersNaam = "Anish"; 
+let adminWachtwoord = "Khedoe";
 
 button.addEventListener("click",
 function()
 {
-    alert(document.getElementById('UserName').value);
+    //alert(document.getElementById('UserName').value);
 
-})
+    if (document.getElementById('UserName').value == adminGebruikersNaam && document.getElementById('Wachtwoord').value == adminWachtwoord) {
+        alert("het klopt");
+        window.location.href ='startPage.html';
+    };
+});
 
 
 
